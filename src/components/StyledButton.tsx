0de9e62485e969beae52354106d7937bdb25f9f4
yk3108 <66248058+yk3108@users.noreply.github.com>
2020-10-01 20:32:07 +0900

    change tsx

---
+++ src/components/StyledButton.tsx
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-use-before-define
+import React from "react";
+import { Col, Button } from "react-bootstrap";
+
+type Props = {
+  col: "3";
+  variant: "primary";
+  id: string;
+  value: string;
+  onClick: () => void;
+};
+
+const StyledButton: React.FC<Props> = (props: Props) => {
+  const { col, variant, id, value, onClick } = props;
+  return (
+    <Col className={`col-${col} p-1`}>
+      <Button variant={variant} id={id} block onClick={() => onClick()}>
+        {value}
+      </Button>
+    </Col>
+  );
+};
+
+export default StyledButton;
