a901e9c73e4d18e13aba9a2d3e12c7f3d2c35254
yk3108 <66248058+yk3108@users.noreply.github.com>
2020-10-01 20:14:29 +0900

    fix for eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// eslint-disable-next-line no-use-before-define
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
